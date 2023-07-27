b5eba29a6c3e381ad4a63d9273697270fd937a10
Alberto Fernández Hernández <45654081+AlbertoUAH@users.noreply.github.com>
2023-07-27 14:49:35 +0200

    Update test_yaml.py

--- wandb/test/test_yaml.py
+++ wandb/test/test_yaml.py
@@ -21,10 +21,10 @@ def test_check_model_existence():
 
 # -- Check if number of epochs is greater than zero
 def test_check_num_epochs():
-	num_epochs = int(config_file['num_train_epochs']['value'])
+	num_epochs = int(config_file['parameters']['num_train_epochs']['value'])
 	assert num_epochs > 0, "Number of epochs must be greater than zero"
 
 # -- Check if convert_to_lowercase parameters is 0 or 1
 def test_check_convert_to_lowercase():
-	convert_to_lowercase = int(config_file['text_to_lowercase']['value'])
+	convert_to_lowercase = int(config_file['parameters']['text_to_lowercase']['value'])
 	assert convert_to_lowercase >= 0 and convert_to_lowercase <= 1, "convert_to_lowercase is a binary variable (0, 1)"
