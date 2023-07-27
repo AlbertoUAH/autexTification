625bf4c2936d023720d077b80c51da4a91f17700
Alberto Fernández Hernández <45654081+AlbertoUAH@users.noreply.github.com>
2023-07-27 14:31:51 +0200

    Update test_yaml.py

--- wandb/test/test_yaml.py
+++ wandb/test/test_yaml.py
@@ -7,7 +7,7 @@ with open('./wandb/test/sweep.yaml', 'r') as yaml_file:
 	config_file = yaml.safe_load(yaml_file)
 
 # -- Check if model & tokenizer exist
-def check_model_existence():
+def test_check_model_existence():
 	model_name = config_file['parameters']['model']['value']
 	try:
 		tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -19,11 +19,11 @@ def check_model_existence():
 		assert exist, "Model and/or tokenizer does not exist"
 
 # -- Check if number of epochs is greater than zero
-def check_num_epochs():
+def test_check_num_epochs():
 	num_epochs = int(config_file['num_train_epochs']['value'])
 	assert num_epochs > 0, "Number of epochs must be greater than zero"
 
 # -- Check if convert_to_lowercase parameters is 0 or 1
-def check_convert_to_lowercase():
+def test_check_convert_to_lowercase():
 	convert_to_lowercase = int(config_file['text_to_lowercase']['value'])
 	assert convert_to_lowercase >= 0 and convert_to_lowercase <= 1, "convert_to_lowercase is a binary variable (0, 1)"
